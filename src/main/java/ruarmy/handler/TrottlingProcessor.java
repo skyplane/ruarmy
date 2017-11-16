25507ab3fd232992698ae1dcf60d28d4733e27d8
kvs <xignusbe>
2017-11-16 16:47:51 +0700

    _

--- src/main/java/ruarmy/handler/TrottlingProcessor.java
+++ src/main/java/ruarmy/handler/TrottlingProcessor.java
@@ -8,6 +8,7 @@ import ruarmy.annotation.Trottling;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.Collectors;
 
 /**
  * Created by skyplane on 16.11.17.
@@ -20,37 +21,26 @@ public class TrottlingProcessor {
 
     @Before("@annotation(ruarmy.annotation.Trottling) && @annotation(trottling)")
     public void before(Trottling trottling){
-
-        if (map.get(trottling.ip())==null) map.put(trottling.ip(), new ConcurrentSkipListSet<>());
-
-        ConcurrentSkipListSet list = map.get(trottling.ip());
-
-        int i = 0;
-
-        for (i = list.size(); i > 0; i--) {
-            if (((Date)list.toArray()[i-1]).getTime() < (new Date().getTime() - trottling.period()*1000)) {
-                break;
+        for (String ip : map.keySet()) {
+            List<Date> toRemove = new ArrayList<>();
+            for (int i = 0; i < map.get(ip).size(); i++) {
+                Date time = (Date) map.get(ip).toArray()[i];
+                if (time.getTime() < (new Date().getTime() - trottling.period() * 1000))
+                toRemove.add(time);
             }
+            map.get(ip).removeAll(toRemove);
+            if (map.get(ip).size()==0)
+                map.remove(ip);
         }
 
-        ConcurrentSkipListSet toRemove = new ConcurrentSkipListSet();
-
-        if (i<list.size())
-            for (int i1 = 0; i1 < i; i1++) {
-                toRemove.add(list.toArray()[i1]);
-            }
-
-        list.removeAll(toRemove);
+        if (map.get(trottling.ip())==null)
+            map.put(trottling.ip(), new ConcurrentSkipListSet<>());
+        ConcurrentSkipListSet list = map.get(trottling.ip());
 
         if (list.size() < trottling.count()) {
             list.add(new Date());
-            map.put(trottling.ip(), list);
         } else {
             throw new AccessDeniedException("403 returned");
         }
-
     }
-
-
-
 }
